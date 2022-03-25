1cb4f35be7cd8f77e4f567140f1bb0a7a5e074ce
Rohan Singh Rawat <rsrofficial99@gmail.com>
2022-03-26 02:36:50 +0530

    [CHANGE]Icon urls changes

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ const App: React.FC = () => {
               mode === "night" ? (
                 <div className='flex space-x-2'>
                   <img src="/icons/day_icon_white.svg" alt="ToggleOn" className='h-2 md:h-4 cursor-pointer' onClick={() => setMode("day")}/>
-                  <img src="/icons/toggle-on-solid_white.svg" alt="ToggleOn" className='h-8 cursor-pointer' onClick={() => setMode("day")}/>
+                  <img src="/icons/toggle-on-solid_white.svg" alt="ToggleOn" className='h-4 md:h-8 cursor-pointer' onClick={() => setMode("day")}/>
                   <img src="/icons/night_icon_white.svg" alt="ToggleOn" className='h-2 md:h-4 cursor-pointer' onClick={() => setMode("night")}/>
                 </div>
               ) : (
@@ -23,15 +23,15 @@ const App: React.FC = () => {
                 </div>
               )
             }
-            <h1 className='hover:animate-bounce cursor-pointer'>Resume</h1>
-            <h1 className='hover:animate-bounce cursor-pointer'>Blog</h1>
+            <h1 className='hover:animate-bounce cursor-pointer' onClick={() => window.open('https://drive.google.com/file/d/1r8surSSejgSw6O52HViRiu9RqzvNT5LA/view?usp=sharing', '_blank', 'noreferrer noopener')}>Resume</h1>
+            <h1 className='hover:animate-bounce cursor-pointer' onClick={() => window.open('https://medium.com/@rsrofficial99/react-vite-tailwind-6238209f95ad', '_blank', 'noreferrer noopener')}>Blog</h1>
           </div>
         </nav>
         <div className='space-y-10'>
           <p className='text-xl md:text-2xl font-semibold'>Hi, I'm</p>
           <h1 className='text-4xl md:text-6xl font-extrabold'>Rohan Singh Rawat</h1>
           <h1 className='text-2xl md:text-4xl font-extrabold'>I do magic with code!</h1>
-          <p className='text-xl md:text-2xl font-semibold'>I am a final year engineering student, trying to create a difference in people's life by writing code. I'm always excited to learn and work on new technologies. Having about 3 years of hustling experience in startup culture.</p>
+          <p className='text-xl md:text-2xl font-semibold'>I am a final year engineering student, trying to create a difference in people's life by writing code. I'm always excited to learn and work on new technologies. Having about 3 years of experience in startup culture. Hustler.</p>
         </div>
         <footer className='flex w-full justify-center'>
           <div className='flex items-center font space-x-16'>
