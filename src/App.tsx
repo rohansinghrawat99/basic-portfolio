c98f09468107b21e8150317f0809e2fab31c323f
Rohan Singh Rawat <rsrofficial99@gmail.com>
2022-03-28 00:48:45 +0530

    [ADDED]Added logos and minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import './App.css';
 import React, { useState } from "react";
 
 const App: React.FC = () => {
-  const [mode, setMode] = useState("night")
+  const [mode, setMode] = useState("day")
   return (
     <div className={`${ mode === 'day' ? 'bg-white text-black': 'bg-black text-white' } h-screen w-screen flex justify-center items-center`}>
       <div className='flex flex-col justify-around h-full w-[80%] min-w-[70%]'>
-        <nav className='flex w-full justify-center md:justify-end'>
+        <nav className='flex flex-col md:flex-row w-full justify-center items-center md:justify-between'>
+          <img src={ `${mode === "night" ? '/icons/logo_white.svg' : '/icons/logo_black.svg'}`}  alt="" className='h-8 mb-10 md:h-14 cursor-pointer' />
           <div className='flex items-center font space-x-10'>
             {
               mode === "night" ? (
@@ -28,16 +29,16 @@ const App: React.FC = () => {
           </div>
         </nav>
         <div className='space-y-10'>
-          <p className='text-xl md:text-2xl font-semibold'>Hi, I'm</p>
-          <h1 className='text-4xl md:text-6xl font-extrabold'>Rohan Singh Rawat</h1>
-          <h1 className='text-2xl md:text-4xl font-extrabold'>I do magic with code!</h1>
-          <p className='text-xl md:text-2xl font-semibold'>I am a final year engineering student, trying to create a difference in people's life by writing code. I'm always excited to learn and work on new technologies. Having about 3 years of experience in startup culture. Hustler.</p>
+          <p className='text-xl md:text-2xl font-semibold'>Hi,  I'm</p>
+          <h1 className={`text-4xl md:text-6xl font-extrabold ${ mode === 'day' ? 'text-black': 'text-neon' } `}>Rohan Singh Rawat</h1>
+          <h1 className='text-2xl md:text-4xl font-extrabold'>I do magic🪄 with code!</h1>
+          <p className='text-xl md:text-2xl font-semibold'>I am a final year engineering student, trying to create a difference in people's life by writing code. I'm always excited to learn and work on new technologies. Having about 3 years of experience in startup culture. Hustler🚀</p>
         </div>
         <footer className='flex w-full justify-center'>
           <div className='flex items-center font space-x-16'>
             <img src={ `${mode === "night" ? '/icons/github_icon_white.svg' : '/icons/github_icon_black.svg'}`} alt="ToggleOn" className='h-4 md:h-8 cursor-pointer' onClick={() => window.open('https://github.com/rohansinghrawat99', '_blank', 'noreferrer noopener')}/>
             <img src={ `${mode === "night" ? '/icons/linkedin_icon_white.svg' : '/icons/linkedin_icon_black.svg'}`} alt="ToggleOn" className='h-4 md:h-8 cursor-pointer' onClick={() => window.open('https://www.linkedin.com/in/rohan-singh-rawat-110022168/', '_blank', 'noreferrer noopener')}/>
-            <img src={ `${mode === "night" ? '/icons/twitter_icon_white.svg' : '/icons/twitter_icon_black.svg'}`} alt="ToggleOn" className='h-4 md:h-8 cursor-pointer' onClick={() =>  window.open('https://twitter.com/rohansrawat', '_blank', 'noreferrer noopener')}/>
+            <img src={ `${mode === "night" ? '/icons/twitter_icon_white.svg' : '/icons/twitter_icon_black.svg'}`} alt="ToggleOn" className='h-4 md:h-7 cursor-pointer' onClick={() =>  window.open('https://twitter.com/rohansrawat', '_blank', 'noreferrer noopener')}/>
           </div>
         </footer>
       </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
+      colors: {
+        neon: "#E6FB04"
+      }
     },
   },
   variants: {
